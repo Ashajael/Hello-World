3224e28ddbe0c150e8bf055fe6d8eb280592252f
Ashajael <carlos.batista@reduc.edu.cu>
2016-03-02 06:28:23 -0800

    uso y trabajo con variables

--- main1.cpp
+++ main1.cpp
@@ -1,7 +1,12 @@
 //something
 #include <iostream>
-int main() 
- { using namespace std;  
-     cout << "Hola mundo" << endl;
-      return 0;
-  }
+int main (void)
+{using namespace std;
+    cout << "Hola mundo"<< endl ; 
+    cout << "test" << endl ; 
+     int a , b , c ;  //declarar variables dentro de la funcion main
+      a = 6 ;  // asignar variables y trabajo de variables
+      b = a ;
+      c = a + b ;
+       cout << c ;
+}
