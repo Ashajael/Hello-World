05aac7706e782516731eaa2f0ca9c87a8ae2179b
Ashajael <carlos.batista@reduc.edu.cu>
2016-03-03 00:29:26 -0500

    http://cpp.sh/3rm4

--- vocaloid.cpp
+++ vocaloid.cpp
@@ -1,5 +1,13 @@
-//minijuego de vocaloid
+// minijuego de vocaloid
 #include <iostream>
-{
- int main (void)
-   
+
+int main ()
+{using namespace std;
+ string voc;
+ cout << "Hola me gustan mucho los vocaloids.\nCual es el nombre de tu favorito?\n";
+ getline (cin,voc);
+ cout << "No me gusta mucho "<<voc<< " prefiero a Honne Dell.\n";
+ cout << "Y hay alguno que te moleste, te caiga mal o te disguste? Me podrias decir su nombre??\n";
+ getline (cin, voc);
+ cout << "Pues a mi me encanta " << voc;
+ }
